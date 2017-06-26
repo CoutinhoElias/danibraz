30b510fb2c3e43c6036a38066683e3752427988c
CoutinhoElias <coutinho.elias@gmail.com>
2017-06-26 16:36:25 -0300

    Adicionando Sweetalert nas mensagens.

---
+++ danibraz/persons/models.py
@@ -0,0 +1,21 @@
+# from django.contrib.auth.models import User
+from django.db import models
+
+
+class Person(models.Model):
+    class Meta:
+        abstract = True
+
+    name = models.CharField(max_length=100)
+    birthday = models.DateField()
+    address = models.CharField(max_length=100)
+    purchase_limit = models.DecimalField(max_digits=15, decimal_places=2)
+
+
+class Client(Person):
+    compra_sempre = models.BooleanField(default=False)
+
+
+class Employee(Person):
+    ctps = models.CharField(max_length=25)
+    salary = models.DecimalField(max_digits=15, decimal_places=2)
