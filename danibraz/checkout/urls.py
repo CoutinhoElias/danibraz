a42fdc4dc7bb5d990a481ca59ad12924b397cb33
CoutinhoElias <coutinho.elias@gmail.com>
2017-12-08 10:32:43 -0300

    Salvando o projeto.

--- danibraz/checkout/urls.py
+++ danibraz/checkout/urls.py
@@ -1,26 +1,17 @@
 from django.conf.urls import url, include
 from django.views.i18n import JavaScriptCatalog
 
-<<<<<<< HEAD
-from danibraz.checkout.views import invoice_list, invoices_create, InvoiceUpdateView
-
-urlpatterns = [
-   # url(r'^pedido/novo/$', InvoiceFormView.as_view(), name='invoice_add'),
-    url(r'^pedido/novo/$', invoices_create, name='invoice_add'),
-=======
-from danibraz.checkout.views import InvoiceFormView, InvoiceUpdateView, invoice_list, InvoiceCreateView
-from danibraz.persons.views import clients_edit
+from danibraz.checkout.views import invoice_list, invoices_create
 
 urlpatterns = [
     #url(r'pedido/novo/$', lancamentos_create, name='lancamentos_create'),
 
     # url(r'pedidos/listar/$', lancamentos, name='lancamento_list'),
     # url(r'pedido/novo/$', lancamentos, name='lancamento'),
-    url(r'^pedido/novo/$', InvoiceFormView.as_view(), name='invoice_add'),
-    url(r'^pedido/novo2/$', InvoiceCreateView.as_view(), name='invoice_add2'),
+
     # url(r'pedido/editar/(?P<invoice_id>\d+)/$', InvoiceUpdateView, name='invoice_edit'),
->>>>>>> 4602188e886e7338a701ed8cec6e40a7379d83c1
-    url(r'^pedido/editar/(?P<pk>\d+)/$$', InvoiceUpdateView.as_view(), name='invoice_edit'),
+    url(r'^new$', invoices_create, name='invoice_add'),
+
     url(r'pedido/listar/$', invoice_list, name='invoice_list'),
 
     url(r'^jsi18n/$', JavaScriptCatalog.as_view(), name='javascript-catalog'),
--- danibraz/checkout/views.py
+++ danibraz/checkout/views.py
@@ -1,25 +1,12 @@
-from django.contrib.messages.views import SuccessMessageMixin
 from django.core.exceptions import ImproperlyConfigured
 from django.db import transaction
-from django.shortcuts import render, redirect, get_object_or_404, render_to_response
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
-from django.template import RequestContext
-from django.urls import reverse_lazy
-<<<<<<< HEAD
-from django.views.generic import FormView, UpdateView
-from extra_views import CreateWithInlinesView, InlineFormSet, NamedFormsetsMixin
 
-from danibraz.checkout.forms import InvoiceForm, ItemInvoiceFormSet, \
-    ItemInvoiceUpdateFormSet, ItemForm, ItemFormSet, InvoiceFormB
-=======
-from django.views.generic import ListView, CreateView, FormView, UpdateView
-from extra_views import CreateWithInlinesView, InlineFormSet, NamedFormsetsMixin
-
-from danibraz.checkout.forms import LancamentoForm, LancamentoItemFormSet, InvoiceForm, ItemInvoiceFormSet, \
-    ItemInvoiceUpdateFormSet, ItemForm
->>>>>>> 4602188e886e7338a701ed8cec6e40a7379d83c1
-from danibraz.checkout.models import Invoice, Item
+from danibraz.checkout.forms import ItemFormSet
+from danibraz.checkout.forms import InvoiceForm
+from danibraz.checkout.models import Invoice
 
 
 #///////////////////////////////////////////////////////////////////////////////////
@@ -62,7 +49,6 @@ def invoices_update(request, pk):
         form = InvoiceForm(instance=invoice)
         formset = ItemFormSet(instance=invoice)
 
-<<<<<<< HEAD
     forms = [formset.empty_form] + formset.forms
     context = {'form': form, 'formset': formset, 'forms': forms}
     return render(request, 'invoices_edit.html', context)
@@ -77,130 +63,6 @@ def invoices_delete(request, pk):
     return render(request, 'invoices_delete.html', {'invoice': invoice})
 
 #///////////////////////////////////////////////////////////////////////////////////
-=======
-class FormActionViewMixin(object):
-    form_action = None
-
-    def get_form_action(self):
-        if not self.form_action:
-            raise ImproperlyConfigured(
-                "%(cls)s is missing a 'form_action'. Define "
-                "%(cls)s.form_action, or override "
-                "%(cls)s.get_form_action()." % {
-                    'cls': self.__class__.__name__
-                }
-            )
-        return self.form_action
-
-    def get_context_data(self, **kwargs):
-        context = super(FormActionViewMixin, self).get_context_data(**kwargs)
-        context['form_action'] = self.get_form_action()
-        return context
-
-
-class ItemInvoiceInlineFormSet(InlineFormSet):
-    model = Item
-    form_class = ItemForm
-    min_num = 1
-    max_num = 16
-    validate_min = True
-    validate_max = True
-    extra = 2
-    can_delete = False
-    items = 'items'
-
-
-class InvoiceCreateView(SuccessMessageMixin, FormActionViewMixin, NamedFormsetsMixin, CreateWithInlinesView):
-    model = Invoice
-    # fields = ['assistido']
-    form_class = InvoiceForm
-    template_name = 'checkout/invoice_form2.html'
-    inlines = [
-        ItemInvoiceInlineFormSet,
-    ]
-    inlines_names = [
-        'iteminvoice_inline'
-    ]
-
-    def get_form_action(self):
-        kwargs = {
-
-        }
-        return reverse_lazy('checkout:invoice_add2', kwargs=kwargs)
-
-    def get_form_kwargs(self):
-        kwargs = super(InvoiceCreateView, self).get_form_kwargs()
-        kwargs['prefix'] = 'invoice'
-        return kwargs
-
-    def get_context_data(self, **kwargs):
-        contexto = super(InvoiceCreateView, self).get_context_data(**kwargs)
-        return contexto
-
-    # repare que esse metodo é diferente "forms_valid" com "s" e não form_valid
-    def forms_valid(self, form, inlines):
-        invoice = form.save(commit=False)
-        total = 0
-        for inline in inlines:
-            for item_form in inline.forms:
-                item = item_form.save(commit=False)
-                total = total + (item.quantity * item.unit_price)
-
-        invoice.total = total
-        # save sera chamado em:
-        return super(InvoiceCreateView, self).forms_valid(form, inlines)
-
-    def get_initial(self):
-        initial = super(InvoiceCreateView, self).get_initial()
-
-        initial.update(
-            {
-            }
-        )
-        return initial
-
-    def get_success_url(self):
-        url = reverse_lazy('checkout:invoice_add2')
-        return url
-
-
-
-
->>>>>>> 4602188e886e7338a701ed8cec6e40a7379d83c1
-class InvoiceUpdateView(SuccessMessageMixin, UpdateView):
-    model = Invoice
-    form_class = InvoiceFormB
-    template_name = 'checkout/invoice_form.html'
-    success_url = reverse_lazy('checkout:invoice_list')
-    success_message = 'The invoice was edited correctly.'
-
-    def get_context_data(self, **kwargs):
-        context = super(InvoiceUpdateView, self).get_context_data(**kwargs)
-        invoice = self.get_object()
-        productos = invoice.nota.all()
-        if self.request.POST:
-            context['formset'] = ItemInvoiceUpdateFormSet(self.request.POST, self.request.FILES, prefix='items')
-        else:
-            context['formset'] = ItemInvoiceUpdateFormSet(queryset=productos, prefix='items')
-        return context
-
-    def form_valid(self, form):
-        context = self.get_context_data()
-        formset = context['formset']
-        total = 0
-        if formset.is_valid():
-            invoice = form.save(commit=False)
-            for item_form in formset.forms:
-                item = item_form.save(commit=False)
-                item.invoice = self.get_object()
-                item.save()
-                total += item.quantity * item.unit_price
-            formset.save()
-            invoice.total = total
-            invoice.save()
-            return super(InvoiceUpdateView, self).form_valid(form)
-        else:
-            return self.render_to_response(self.get_context_data(form=form))
 
 
 def invoice_list(request):
